b27002045fd845a04e6dbf54c1fe6e0caa4594e2
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-07-26 14:51:44 -0400

    didnt work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require foundation
 //= require foundation.min
 //= require_tree .
 
 $(function() {
   $(document).foundation('topbar', 'reflow');
 });
+
+
+
