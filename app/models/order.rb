0de9f5d4ca5c4a89d7ae05ce127d14137fd22cf8
Anuradha Srikanth <asrikant@andrew.cmu.edu>
2017-07-31 17:53:05 +0530

    started dataviz for home

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,6 +20,7 @@ class Order < ActiveRecord::Base
   scope :chronological, -> { order(date: :desc) }
   scope :paid,          -> { where.not(payment_receipt: nil) }
   scope :for_school,    ->(school_id) { where(school_id: school_id) }
+  scope :for_month,     ->(month) {"date= ?", month}
 
   # Class methods
   def self.not_shipped
